d0649d915def471b443c320da853a957bbeb310f
Kris Albert Lee <krisalbert@gmail.com>
2015-07-13 21:08:37 -0700

    second commit

--- problem02.rb
+++ problem02.rb
@@ -1,24 +1,27 @@
 nums  = [0]
 fib = 1
+m=0
 
-for m in 0...33
+currentNumber = 0
+limit = 4000000
+
+while currentNumber < limit do
     if m < 1
         fib = fib + nums[m]
     else
         fib = nums[m-1] + nums[m]
     end
-    
-        nums.push(fib)
+    nums.push(fib)
+    currentNumber = fib
     m+=1
 end
-puts nums
-puts "***************"
+#puts nums
+#puts "***************"
 
 sum= 0
-
 nums.keep_if { |a| a%2 ==0 }
 nums.each { |a| sum +=a }
-puts nums
+#puts nums
 puts sum
 
 #answer is 4613732
